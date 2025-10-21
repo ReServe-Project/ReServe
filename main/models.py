9a282b946cdb3ce77ea01e544bfa7096041cf439
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2025-10-21 19:50:51 +0700

    created main app

---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class PersonalGoal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({'Done' if self.is_completed else 'Pending'})"
+
+class user (models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    
