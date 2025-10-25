c05e19bb6b48ff796a20b13b76da14a28a50da52
maharanianindya <anindyabudiarti@gmail.com>
2025-10-25 22:38:38 +0700

    Home_search: fix create permission + CRUD + detail fields

--- ReServe/settings.py
+++ ReServe/settings.py
@@ -158,6 +158,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = "main:login"
-LOGIN_REDIRECT_URL = "blog:show_blog"
-LOGOUT_REDIRECT_URL = "blog:show_blog"
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "profile_view"
+LOGOUT_REDIRECT_URL = "login"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- home_search/models.py
+++ home_search/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
 from accounts.models import User
-
+from django.conf import settings
 
 CATEGORY_CHOICES = [
     ("yoga","Yoga"), ("pilates","Pilates"), ("dance","Dance"),
@@ -9,16 +8,24 @@ CATEGORY_CHOICES = [
 ]
 
 class Class(models.Model):
-    # baris dari Excel atau buatan user -> dibedakan dari 'owner'
-    owner       = models.ForeignKey(User, null=True, blank=True,
-                                    on_delete=models.SET_NULL, related_name="my_classes")
+    owner       = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name="my_classes")
     name        = models.CharField(max_length=200)
     category    = models.CharField(max_length=50, choices=CATEGORY_CHOICES, db_index=True)
     price       = models.IntegerField(default=0)
     image_url   = models.URLField(blank=True)
     description = models.TextField(blank=True)
 
+
+    datetime   = models.DateTimeField(null=True, blank=True)
+    location    = models.CharField(max_length=200, blank=True)
+
     def __str__(self): return self.name
+
+    @property
+    def hero_image(self) -> str:
+        return self.image_url or ""
+
+
 
 
 
