b605316c9dd41050b9b68402fc2023cf1ec9ed13
maharanianindya <anindyabudiarti@gmail.com>
2025-10-25 22:38:38 +0700

    Home_search: fix create permission + CRUD + detail fields

--- home_search/urls.py
+++ home_search/urls.py
@@ -6,4 +6,8 @@ app_name = "home_search"
 urlpatterns = [
     path("", views.home, name="home"),
     path("search/", views.search, name="search"),
+    path("classes/create/", views.ClassCreateView.as_view(), name="class_create"),
+    path("classes/<int:pk>/", views.ClassDetailView.as_view(), name="class_detail"),
+    path("classes/<int:pk>/edit/", views.ClassUpdateView.as_view(), name="class_edit"),
+    path("classes/<int:pk>/delete/", views.ClassDeleteView.as_view(), name="class_delete"),
 ]
