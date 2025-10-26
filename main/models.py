e7d42ff5ad6ba7b472f2f0b0f5ec4ab02a36e3ce
kerangleeya <shelia.vellicita13@gmail.com>
2025-10-26 18:58:06 +0700

    Newest master merge

--- main/models.py
+++ main/models.py
@@ -23,9 +23,8 @@ class PersonalGoal(models.Model):
     def __str__(self):
         return f"{self.title} ({self.date})"
 
-class Blog(models.Model):
+class Blogs(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
     title = models.CharField(max_length=255)
     content = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
