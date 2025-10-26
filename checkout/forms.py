6d10a07a43ec0433a068ee529179081294f098e6
EvelynOct <evelyne.aritonang72@gmail.com>
2025-10-26 20:37:22 +0700

    Updating the css files

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,10 +1,21 @@
-# checkout/forms.py
-
 from django import forms
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        # Only include the fields shown in the design
-        fields = ['full_name', 'email', 'phone_number']
+        # These are the fields from your model
+        fields = ['full_name', 'email', 'phone_number'] 
+        
+        # This part adds the CSS class to each field
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder': 'Enter your full name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'user@gmail.com'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Ex: 081234567890'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # This loop adds your new CSS class to all fields
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
