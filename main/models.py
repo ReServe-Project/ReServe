d36a88f5983113b7aea0dd23bdb33e363da25e4d
kerangleeya <shelia.vellicita13@gmail.com>
2025-10-26 16:10:56 +0700

    Edit models

--- main/models.py
+++ main/models.py
@@ -19,12 +19,13 @@ class PersonalGoal(models.Model):
     def __str__(self):
         return f"{self.title} ({self.date})"
 
-class Blogs(models.Model):
+class Blog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
     title = models.CharField(max_length=255)
     content = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.title
