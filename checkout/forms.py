8a45038f4e0045226d545475d4816324702c9847
kerangleeya <shelia.vellicita13@gmail.com>
2025-12-03 19:04:27 +0700

    Finalization

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,20 +1,7 @@
 from django import forms
-from django.core.validators import RegexValidator
 from .models import Booking
 
-phone_validator = RegexValidator(regex=r'^\d{11}$', message='Phone number must be exactly 11 digits.')
-
 class BookingForm(forms.ModelForm):
-    phone_number = forms.CharField(
-        max_length=11,
-        validators=[phone_validator],
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Ex: 81234567890',
-            'inputmode': 'numeric',
-            'pattern': r'\d{11}'
-        })
-    )
-
     class Meta:
         model = Booking
         # These are the fields from your model
@@ -24,6 +11,7 @@ class BookingForm(forms.ModelForm):
         widgets = {
             'full_name': forms.TextInput(attrs={'placeholder': 'Enter your full name'}),
             'email': forms.EmailInput(attrs={'placeholder': 'user@gmail.com'}),
+            'phone_number': forms.NumberInput(attrs={'placeholder': 'Enter your phone number'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -31,11 +19,3 @@ class BookingForm(forms.ModelForm):
         # This loop adds your new CSS class to all fields
         for field_name in self.fields:
             self.fields[field_name].widget.attrs['class'] = 'form-control'
-
-    def clean_phone_number(self):
-        pn = self.cleaned_data.get('phone_number', '') or ''
-        # remove non-digit characters (in case user types spaces/dashes)
-        digits = ''.join(ch for ch in pn if ch.isdigit())
-        if len(digits) != 11:
-            raise forms.ValidationError('Phone number must contain exactly 11 digits.')
-        return digits
